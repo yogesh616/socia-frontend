63b4cd8f1c32e1019090c4f087f26867223a2606
Yogesh Saini <71480544+yogesh616@users.noreply.github.com>
2025-10-28 09:41:33 +0530

    Update API_URL to new chat server endpoint

--- src/constant/api.js
+++ src/constant/api.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://socia-server-yd7t.onrender.com/api" || "http://localhost:3000/api";
+export const API_URL = "https://socia-chat-server.onrender.com/api" || "http://localhost:3000/api";
